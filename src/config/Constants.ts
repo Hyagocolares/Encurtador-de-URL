3118bd37e1a2f56c05e64c0c870e17e9a59b1302
Hyagocolares <hyagocolares15@gmail.com>
2022-06-11 19:42:54 -0300

    primeiro commit deste repositorio

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:3000',
+	MONGO_CONNECTION:
+		'mongodb+srv://hyagocolares:GdEQbvukNLjez2Z@url.valmxvh.mongodb.net/?retryWrites=true&w=majority',
+}
